c30fc3c77d72d5458b63a2ad6b3b423dfe53ec40
Simone Bacchio <s.bacchio@gmail.com>
2020-01-14 13:42:52 +0200

    Adding lyncs

---
+++ lyncs/__init__.py
@@ -0,0 +1,4 @@
+"""
+Lyncs, a python API for LQCD applications
+"""
+import lyncs_config as config
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+try:
+    import lyncs_config
+except ModuleNotFoundError:
+    # TODO: run cmake with default options and then import lyncs_config
+    raise
+
+setup(name='lyncs',
+      version=lyncs_config.version,
+      packages=find_packages(),
+      description='A python API for LQCD applications',
+      long_description=open("./README.md", 'r').read(),
+      long_description_content_type="text/markdown",
+      keywords="LQCD, API, ...",
+      classifiers=["Intended Audience :: Students",
+                   "Intended Audience :: Researchers",
+                   "License :: OSI Approved :: "
+                   "The 3-Clause BSD License (BSD-3-Clause)",
+                   "Natural Language :: English",
+                   "Programming Language :: C",
+                   "Programming Language :: C++",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3.6"],
+      license='BSD-3-Clause'
+    )
