3bca67493969969f9453832fb20b4e173f82f681
Simone Bacchio <s.bacchio@gmail.com>
2020-01-15 17:16:36 +0200

    replacing LIBRARY_PATH with PATH

--- CMakeExt/TMLQCD.cmake
+++ CMakeExt/TMLQCD.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 
-if(NOT TMLQCD_LIBRARY_PATH)
-  message(STATUS "No TMLQCD_LIBRARY_PATH given; we will download it.")
+if(NOT TMLQCD_PATH)
+  message(STATUS "No TMLQCD_PATH given; we will download it.")
   set(EXTERNAL_INSTALL_LOCATION ${CMAKE_BINARY_DIR}/external)
   ExternalProject_Add(tmLQCD
     GIT_REPOSITORY https://github.com/etmc/tmLQCD
@@ -11,12 +11,12 @@ if(NOT TMLQCD_LIBRARY_PATH)
     BUILD_IN_SOURCE 1
     INSTALL_COMMAND make -j install
   )
-  set(TMLQCD_LIBRARY_PATH ${EXTERNAL_INSTALL_LOCATION})
+  set(TMLQCD_PATH ${EXTERNAL_INSTALL_LOCATION})
   set(TMLQCD_INSTALL ON)
 endif()
 
 message(STATUS "Searching for tmLQCD library")
-if(EXISTS "${TMLQCD_LIBRARY_PATH}/lib/libhmc.so" AND EXISTS "${TMLQCD_LIBRARY_PATH}/include/hmc.h")
+if(EXISTS "${TMLQCD_PATH}/lib/libhmc.so" AND EXISTS "${TMLQCD_PATH}/include/hmc.h")
   set(TMLQCD_FOUND ON)
 endif()
 
