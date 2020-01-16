c4d91cea78dfb83b61b3dafad80552203e538d93
Simone Bacchio <s.bacchio@gmail.com>
2020-01-16 14:14:51 +0200

    Udating __init__.py

--- lyncs/__init__.py
+++ lyncs/__init__.py
@@ -2,3 +2,9 @@
 Lyncs, a python API for LQCD applications
 """
 import lyncs_config as config
+
+if config.mpi_enabled:
+    from lyncs import mpi
+    
+if config.ddalphaamg_enabled:
+    from lyncs import DDalphaAMG
