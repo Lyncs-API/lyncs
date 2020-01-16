3ef659108dbdcb1ddcda57b28ae500a5ea36535f
Simone Bacchio <s.bacchio@gmail.com>
2020-01-16 14:14:05 +0200

    Adding MPI in python

---
+++ lyncs/mpi/__init__.py
@@ -0,0 +1,25 @@
+from lyncs_config import mpi_enabled as enabled
+assert enabled, "MPI not enabled. Cannot import it"
+
+_lib = None
+def get_lib():
+    global _lib
+    if not _lib:
+        import cppyy
+        import lyncs_config as config
+        cppyy.add_include_path(config.mpi_include)
+        cppyy.include("mpi.h")
+        cppyy.load_library(config.mpi_libraries)
+        _lib = cppyy.gbl
+    return _lib
+
+def initialized():
+    from ctypes import c_int
+    u = c_int(0)
+    get_lib().MPI_Initialized(u)
+    return bool(u)
+
+def initialize():
+    assert not initialized()
+    from cppyy import nullptr
+    get_lib().MPI_Init(nullptr, nullptr)
