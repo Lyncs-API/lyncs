6a8af3875d5f8dfe88af666d1dda1b019e5c8bce
Simone Bacchio <s.bacchio@gmail.com>
2020-01-28 17:22:03 +0200

    Testing initialization

--- tests/test_DDalphaAMG.py
+++ tests/test_DDalphaAMG.py
@@ -1,5 +1,8 @@
 import lyncs
 def test_solver():
     if not lyncs.config.ddalphaamg_enabled: return
-    solver = lyncs.DDalphaAMG.solver()
-    assert solver
+    client = lyncs.mpi.Client(num_workers=1)
+    comms = client.create_comm(actor=False)
+    _solvers = client.map(lyncs.DDalphaAMG._Solver,comms, procs=[1,1,1,1], actor=True)
+    _actors = [s.result() for s in _solvers]
+    assert all([s.done() for s in _solvers])
