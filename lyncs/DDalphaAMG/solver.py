f2725d09a9b567926944ea06e177acf667891ae8
Simone Bacchio <s.bacchio@gmail.com>
2020-01-28 17:19:32 +0200

    Updating interface

--- lyncs/DDalphaAMG/solver.py
+++ lyncs/DDalphaAMG/solver.py
@@ -1,23 +1,28 @@
 from lyncs.DDalphaAMG import get_lib
 
-class solver:
+class _Solver:
     "The DDalphaAMG solver class"
-    def __init__(self):
+    def __init__(self, comm, procs=None):
         from cppyy import nullptr
+        import cppyy.ll
+        from mpi4py import MPI
         self.init_params = get_lib().DDalphaAMG_init()
+        self.run_params = get_lib().DDalphaAMG_parameters()
+        self.status = get_lib().DDalphaAMG_status()
         
-        #self.init_params.comm_cart
+        self.init_params.comm_cart = cppyy.ll.cast["MPI_Comm*"](MPI._addressof(comm))
         self.init_params.Cart_rank = nullptr
         self.init_params.Cart_coords = nullptr
 
         for i in range(4):
-            self.init_params.global_lattice[i] = 1
-            self.init_params.procs[i] = 1
+            self.init_params.global_lattice[i] = 8
+            self.init_params.procs[i] = procs[i]
 
-            self.init_params.block_lattice[i] = 1
+            self.init_params.block_lattice[i] = 4
             
-            self.init_params.theta[i] = 1
+            self.init_params.theta[i] = 0
 
+        
         self.init_params.bc = 0
 
         self.init_params.number_of_levels = 2
@@ -29,7 +34,8 @@ class solver:
 
         #self.init_params.init_file = nullptr
         #self.init_params.rnd_seeds = nullptr
-        
+
+        get_lib().DDalphaAMG_initialize(self.init_params, self.run_params, self.status)
         
 
         
