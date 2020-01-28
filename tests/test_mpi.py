b0c5bc16d7103c9b4cdec551b8d5c3be0ff681cb
Simone Bacchio <s.bacchio@gmail.com>
2020-01-28 17:21:51 +0200

    Adding other test

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -3,6 +3,10 @@ def test_client():
     client = Client(num_workers=1)
     assert client._server is not None
     assert len(client.workers) == 1
+
+    comms = client.create_comm(actor = True)
+    comms = [comm.result() for comm in comms]
+    assert [comm.rank for comm in comms] == [0]
     
     client.close_server()
     assert client._server is None
