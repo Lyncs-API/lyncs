b68d38512ca99ac563b0b961062232bf666ffc5a
Simone Bacchio <s.bacchio@gmail.com>
2020-02-02 11:23:55 +0200

    Improving default repr

--- lyncs/utils.py
+++ lyncs/utils.py
@@ -8,10 +8,13 @@ def default_repr(self):
     """
     ret = type(self).__name__+"("
     pad = " "*(len(ret))
-    for arg in self.__init__.__code__.co_varnames[1:]:
+    nvars = len(self.__init__.__code__.co_varnames)
+    for i in range(1,nvars):
+        arg = self.__init__.__code__.co_varnames[i]
         if hasattr(self, arg):
             arg_eq = arg+" = "
             val = repr(getattr(self,arg)).replace("\n", "\n"+pad+" "*(len(arg_eq)))
-            ret += arg+" = "+val+",\n"+pad
-    ret+=")"
+            ret += arg+" = "+val
+            if i < nvars-1: ret += ",\n"+pad
+    ret += ")"
     return ret
