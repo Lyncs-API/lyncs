751e78f5e6c975d007c2048f2c40cb5a0569ff5f
Simone Bacchio <s.bacchio@gmail.com>
2020-02-17 11:40:53 +0200

    Improving default repr

--- lyncs/utils.py
+++ lyncs/utils.py
@@ -6,15 +6,16 @@ def default_repr(self):
     """
     Default repr used by lyncs
     """
+    from inspect import signature
     ret = type(self).__name__+"("
     pad = " "*(len(ret))
     found_first = False
-    for arg in self.__init__.__code__.co_varnames:
-        if arg in dir(self):
+    for key,val in signature(self.__init__).parameters.items():
+        if key in dir(self) and val.kind == val.POSITIONAL_OR_KEYWORD:
             if found_first: ret += ",\n"+pad
             else: found_first = True
-            arg_eq = arg+" = "
-            val = repr(getattr(self,arg)).replace("\n", "\n"+pad+" "*(len(arg_eq)))
-            ret += arg+" = "+val
+            arg_eq = key+" = "
+            val = repr(getattr(self,key)).replace("\n", "\n"+pad+" "*(len(arg_eq)))
+            ret += key+" = "+val
     ret += ")"
     return ret
