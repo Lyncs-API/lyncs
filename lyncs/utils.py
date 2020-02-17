899897404917d072d522143fa5465c8f58f35876
Simone Bacchio <s.bacchio@gmail.com>
2020-02-17 13:06:52 +0200

    Printing without name the positional options

--- lyncs/utils.py
+++ lyncs/utils.py
@@ -6,16 +6,25 @@ def default_repr(self):
     """
     Default repr used by lyncs
     """
-    from inspect import signature
+    from inspect import signature, _empty
     ret = type(self).__name__+"("
     pad = " "*(len(ret))
     found_first = False
     for key,val in signature(self.__init__).parameters.items():
-        if key in dir(self) and val.kind == val.POSITIONAL_OR_KEYWORD:
+        if key in dir(self) and val.kind in [val.POSITIONAL_ONLY,
+                                             val.POSITIONAL_OR_KEYWORD,
+                                             val.KEYWORD_ONLY]:
+            if val.kind == val.POSITIONAL_ONLY:
+                arg_eq = ""
+            elif val.kind == val.POSITIONAL_OR_KEYWORD and val.default == _empty:
+                arg_eq = ""
+            else:
+                arg_eq = key+" = "
+                
             if found_first: ret += ",\n"+pad
             else: found_first = True
-            arg_eq = key+" = "
+            
             val = repr(getattr(self,key)).replace("\n", "\n"+pad+" "*(len(arg_eq)))
-            ret += key+" = "+val
+            ret += arg_eq+val
     ret += ")"
     return ret
