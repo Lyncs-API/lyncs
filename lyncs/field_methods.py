f4807b063003515b55e217bb873a715066b7db68
Simone Bacchio <s.bacchio@gmail.com>
2020-02-26 17:01:51 +0200

    Adding reshaping functions

--- lyncs/field_methods.py
+++ lyncs/field_methods.py
@@ -17,13 +17,101 @@ class FieldMethods:
     def __pos__(self):
         return self
     
-    def __matmul__(self, other):
-        pass
-
-    def __rmatmul__(self, other):
-        pass
     
+    def __matmul__(self, other):
+        return matmul(self,other)
+    
+    
+    def __rmatmul__(self, other):
+        return matmul(other, self)
+    
+    
+    def reshape(self, *axes_order):
+        """
+        Changes the axes_order of the field.
+        
+        *NOTE*: this is conceptually different from numpy.reshape.
+        """
+        from .field import Field
+        return Field(self, axes_order = axes_order)
+    
+    
+    @property
+    def T(self):
+        "Transposes the field."
+        return self.transpose()
+    
+    
+    def transpose(self, *axes, **axes_order):
+        """
+        Transposes the matrix/tensor indeces, i.e. the dimensions that are repeated.
+        
+        *NOTE*: this is conceptually different from numpy.transpose.
+        
+        Parameters
+        ----------
+        axes: str
+            If given, only the listed axes are transposed, otherwise all the tensorial axes are changed.
+            By default the order of the indeces is inverted.
+        axes_order: dict
+            Same as axes, but specifying the reordering of the indeces. The keys must be "*axis*_order".
+        """
+        
+        axes = list(axes)
+        if not axes and not axes_order:
+            axes = self.axes
+        elif axes_order:
+            for key in axes_order:
+                assert key.endswith("_order"), "Keys in axes_order must end with \"_order\""
+                axes.append(key[:-6])
 
+        new_order = {}
+        for key in set(axes):
+            if key not in self.axes:
+                assert key in self.dimensions, "Unknown axes %s" % key
+                keys = self._expand(key)
+            else:
+                keys = [key]
+
+            for key2 in keys:
+                count = self.axes.count(key2)
+                if key+"_order" in axes_order:
+                    from .tunable import Permutation
+                    assert Permutation(list(range(count))).compatible(axes_order[key+"_order"]), """
+                    The axes_order of %s must be a permutation of %s.
+                    """ %(key, list(range(count)))
+
+                    if count > 1:
+                        new_order[key2+"_order"] = axes_order[key+"_order"]
+                elif count > 1:
+                    new_order[key2+"_order"] = reversed(getattr(self, key2+"_order"))
+
+        if new_order:
+            return Field(self, **new_order)
+        else:
+            return self
+
+    @property
+    def H(self):
+        "Conjugate transpose of the field."
+        return self.dagger()
+    
+    
+    def dagger(self, *axes, **axes_order):
+        """
+        Conjugate and transposes the matrix/tensor indeces, i.e. the dimensions that are repeated.
+        
+        Parameters
+        ----------
+        axes: str
+            If given, only the listed axes are transposed, otherwise all the tensorial axes are changed.
+            By default the order of the indeces is inverted.
+        axes_order: dict
+            Same as axes, but specifying the reordering of the indeces. The keys must be "*axis*_order".
+        """
+        return self.transpose(*axes, **axes_order).conj()
+
+# The following are simple universal functions and they are dynamically wrapped
 
 ufuncs = (
     # math operations
@@ -150,14 +238,11 @@ operators = (
 )
 
 
-def check(*fields):
-    # TODO
-    return True
-
-
 def prepare(*fields):
+    from .field import Field
+    assert all([isinstance(field, Field) for field in fields])
     # TODO
-    return fields, fields[0]
+    return fields, Field(fields[0])
 
 
 def wrap_ufunc(ufunc):
@@ -176,24 +261,10 @@ def wrap_ufunc(ufunc):
 
         # Uniforming the fields involved
         fields = [(i,arg) for i,arg in enumerate(args) if isinstance(arg, Field)]
-        assert check(*[field for i,field in fields]), "Checks on the fields failed"
-        
-        delay = any([isinstance(field.field, Delayed) for i,field in fields])
-        if len(fields) > 1:
-            if delay:
-                res = delayed(prepare)(*[field for i,field in fields])
-                res._length = 2
-                new_fields = res[0]
-                out_field = res[1]
-                new_fields._length = len(fields)
-            else:
-                new_fields, out_field = prepare(*[field for i,field in fields])
-            for (i,old), new in zip(fields, new_fields):
-                assert args[i] is old, "Trivial assertion" % (old, args[i])
-                args[i] = new.field
-        else:
-            out_field = args[0]    
-            args = [arg.field if isinstance(arg, Field) else arg for arg in args]
+        new_fields, out_field = prepare(*[field for i,field in fields])
+        for (i,old), new in zip(fields, new_fields):
+            assert args[i] is old, "Trivial assertion" % (old, args[i])
+            args[i] = new.field
         
         # Calling ufunc
         delay = any([isinstance(arg, Delayed) for arg in args])
@@ -208,10 +279,10 @@ def wrap_ufunc(ufunc):
                 field.field = res[i] 
             return fields
         else:
-            field = Field(out_field, dtype=trial.dtype)
-            if delay: field.field = delayed(ufunc)(*args, **kwargs)
-            else: field.field = ufunc(*args, **kwargs)
-            return field
+            out_field.dtype = trial.dtype
+            if delay: out_field.field = delayed(ufunc)(*args, **kwargs)
+            else: out_field.field = ufunc(*args, **kwargs)
+            return out_field
     return wrapped
 
 
