1144ee40bac48b057414dd2ae6f11fdb79926d62
Simone Bacchio <s.bacchio@gmail.com>
2020-03-11 19:23:38 +0200

    Correcting plaquette

--- lyncs/fields/gauge_links.py
+++ lyncs/fields/gauge_links.py
@@ -21,14 +21,16 @@ def __init__(self, **kwargs):
 
 
 def plaquette(self, dirs=None):
-    dirs = set(dirs or self.dims)
-    assert dirs.issubset(self.dims), "Dims not part of the field"
+    dirs = dirs or self.dims
+    assert set(dirs).issubset(self.dims), "Dims not part of the field"
     assert len(dirs) > 1, "At least two dims needed for computing plaquette"
 
     plaq = 0
-    for dir1 in dirs:
-        for dir2 in dirs:
-            if dir1!=dir2:
-                plaq += (self[dir1] @ self[dir2].roll(dir1, 1) @ \
-                         self[dir1].roll(dir2, 1).H @ self[dir2].H).trace().sum()
-    return plaq
+    count = 0
+    for i,dir1 in enumerate(dirs[:-1]):
+        for dir2 in dirs[i+1:]:
+            plaq += self[dir1].dot(self[dir2].roll(dir1, -1),  self[dir1].roll(dir2, -1).H,
+                                   self[dir2].H).trace().real
+            count+=1
+            
+    return plaq.mean()/count/self.lattice["color"]
