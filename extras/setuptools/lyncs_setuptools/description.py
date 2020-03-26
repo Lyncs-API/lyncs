d284d4cc3b90b4e5500ea681fcc652343621cd63
Simone Bacchio <s.bacchio@gmail.com>
2020-03-26 11:05:42 +0200

    Adding evaluation of [lyncs_setuptools]

--- extras/setuptools/lyncs_setuptools/description.py
+++ extras/setuptools/lyncs_setuptools/description.py
@@ -1,3 +1,8 @@
+__all__ = [
+    "find_description",
+    ]
+
+_ALREADY_RUNNING = False
 
 def find_description(readme=None):
     import codecs, os
@@ -29,11 +34,30 @@ def find_description(readme=None):
         
     with codecs.open(readme, encoding='utf-8') as f:
         add_to_data_files(readme, directory=".")
-        dlong = f.read()
+        dlong = f.read().split("\n")
 
+    for i,line in enumerate(dlong):
+        if line.startswith("[lyncs_setuptools]: "):
+            global _ALREADY_RUNNING
+
+            if not _ALREADY_RUNNING:
+                _ALREADY_RUNNING = True
+                line=": ".join(line.split(":")[1:])
+                import io
+                from contextlib import redirect_stdout
+                
+                f = io.StringIO()
+                with redirect_stdout(f):
+                    for line in line.split(";"):
+                        exec(line.strip())
+                dlong[i] = f.getvalue()#.replace("\\n", "\n")
+                
+                _ALREADY_RUNNING = False
+            else:
+                dlong[i] = ""
+                
     dshort = ""
-    
-    for line in dlong.split("\n"):
+    for line in dlong:
         if line.split():
             dshort = line
             break
@@ -41,8 +65,9 @@ def find_description(readme=None):
     if "markdown" in dtype:
         while dshort.startswith("#"):
             dshort = dshort[1:]
+    
 
-    return dshort.strip(), dlong, dtype
+    return dshort.strip(), "\n".join(dlong), dtype
         
         
     
