16abd800905d921fcc81b4f1b480b455258f3857
Simone Bacchio <s.bacchio@gmail.com>
2020-03-26 11:05:03 +0200

    Adding main

--- extras/setuptools/lyncs_setuptools/__init__.py
+++ extras/setuptools/lyncs_setuptools/__init__.py
@@ -4,6 +4,12 @@ from .description import *
 from .classifiers import *
 from .cmake import *
 
+# TODO: authomatize the following
+AUTHOR='Simone Bacchio'
+AUTHOR_EMAIL='s.bacchio@gmail.com'
+URL='https://github.com/sbacchio'
+
+
 def setup(name, **kwargs):
     from setuptools import setup, find_packages
 
@@ -29,3 +35,49 @@ def setup(name, **kwargs):
 
     setup(name=name, **kwargs)
     
+    
+switcher = {
+    "author": AUTHOR,
+    "author_email": AUTHOR_EMAIL,
+    "url": URL,
+    "version": find_version,
+    "description": lambda: find_description()[0],
+    "long_description_content_type": lambda: find_description()[2],
+    "long_description": lambda: find_description()[1],
+    "classifiers": classifiers,
+    "data_files": get_data_files,
+}
+
+
+def main():
+    import sys
+    assert len(sys.argv)==1 or (len(sys.argv)==2 and sys.argv[1] in switcher.keys()), """
+    Only one argument is allowed and options are '%s'.
+    """ % ("', '".join(switcher.keys()))
+
+    def run(arg):
+        if callable(arg):
+            return arg()
+        else:
+            return arg
+
+    if len(sys.argv)==2:
+        return run(switcher[sys.argv[1]])
+    else:
+        accum = ""
+        for key, fnc in switcher.items():
+            res = run(fnc)
+            
+            if type(res) is str and "\n" in res:
+                res = "\"\"\"\n" + res + "\n\"\"\""
+            elif type(res) is str:
+                res = "\"" + res + "\""
+            elif type(res) is list and res:
+                res = "[\n" + ",\n".join((repr(i) for i in res)) + "\n]"                
+            else:
+                res = repr(res)
+                
+            res = res.replace("\n", "\n |  ")
+            accum += "%s: %s\n\n" % (key, res)
+        return accum
+            
---
+++ extras/setuptools/lyncs_setuptools/__main__.py
@@ -0,0 +1,5 @@
+from . import main
+
+import sys
+sys.exit(print(main()))
+
