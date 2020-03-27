a26867d53e384098f6d650e20851dd6f1c65dc5d
Simone Bacchio <s.bacchio@gmail.com>
2020-03-27 16:19:02 +0200

    adding classifiers

--- extras/setuptools/lyncs_setuptools/classifiers.py
+++ extras/setuptools/lyncs_setuptools/classifiers.py
@@ -5,10 +5,14 @@ Ref. to https://pypi.org/classifiers/
 
 classifiers = [
     "Development Status :: 2 - Pre-Alpha",
-    "Environment :: Plugins",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
     "Intended Audience :: Science/Research",
     "License :: OSI Approved :: BSD License",
+    "Natural Language :: English",
     "Operating System :: Unix",
+    "Programming Language :: C",
+    "Programming Language :: C++",
     "Programming Language :: Python :: 3 :: Only",
     "Topic :: Scientific/Engineering :: Physics",
 ]
