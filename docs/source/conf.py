d08de06ab39f705d5fd8b1217fdf8c979bb5a35c
Simone Bacchio <s.bacchio@gmail.com>
2020-03-27 17:32:38 +0200

    Giving master_doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Simone Bacchio'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.0'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
