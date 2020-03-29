cca2b85b46c870bfe09b13a7f195eb319a1cda1c
Simone Bacchio <s.bacchio@gmail.com>
2020-03-29 20:22:23 +0300

    Cleaning __init__

--- extras/setuptools/lyncs_setuptools/__init__.py
+++ extras/setuptools/lyncs_setuptools/__init__.py
@@ -1,18 +1,19 @@
 import sys
 import os
 import pathlib
+import codecs
 from setuptools import find_packages
-from setuptools import setup as _setup
+from setuptools import setup as _SETUP
 from .version import *
 from .data_files import *
 from .description import *
 from .classifiers import *
 from .cmake import *
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 
-def setup(*args, **kwargs):
+def complete_kwargs(*args, **kwargs):
     if args:
         assert len(args) == 1, "Only one arg allowed and it will be threated as name."
         assert "name" not in kwargs, "Repeated name parameter"
@@ -57,48 +58,56 @@ def setup(*args, **kwargs):
 
     kwargs["data_files"] += get_data_files()
 
-    if __name__ != "__main__" and not __main__:
-        _setup(**kwargs)
-    else:
-        global __argv__
-
-        if len(__argv__) == 1:
-            assert __argv__[0] in kwargs, "Allowed options are '%s'" % (
-                "', '".join(kwargs)
-            )
-            print(kwargs[__argv__[0]])
-        else:
-            for key, res in kwargs.items():
-                if isinstance(res, str) and "\n" in res:
-                    res = '"""\n' + res + '\n"""'
-                elif isinstance(res, str):
-                    res = '"' + res + '"'
-                elif isinstance(res, list) and res:
-                    res = "[\n" + ",\n".join((repr(i) for i in res)) + "\n]"
-                else:
-                    res = repr(res)
-
-                res = res.replace("\n", "\n |  ")
-                if not __argv__ or key in __argv__:
-                    print("%s: %s\n" % (key, res))
+    return kwargs
 
 
-__argv__ = []
-__main__ = False
+def setup(*args, **kwargs):
+    return _SETUP(**complete_kwargs(*args, **kwargs))
 
 
-def main(argv=None):
-    global __main__
-    global __argv__
-    __main__ = True
-
-    if argv is None:
-        __argv__ = sys.argv[1:]
-    else:
-        __argv__ = argv
+def get_kwargs():
+    "Returns the complete set of kwargs passed to setup by calling setup.py"
 
     try:
-        sys.path.insert(0, os.getcwd())
-        import setup as _setup
+        global _SETUP
+        _tmp = _SETUP
+        ret = dict()
+        _SETUP = ret.update
+        with codecs.open("setup.py", encoding="utf-8") as _fp:
+            exec(_fp.read())
+        _SETUP = _tmp
+        return ret
     except BaseException:
-        setup()
+        return complete_kwargs()
+
+
+def print_keys(keys=None):
+    """
+    Prints all or part of the kwargs given to setup by calling setup.py.
+
+    Parameters
+    ----------
+    keys: list
+      List of keys to print. If empty all of them are printed.
+      Note: if None is given, then sys.argv is used
+    """
+    keys = sys.argv[1:] if keys is None else ([keys] if isinstance(keys, str) else keys)
+    kwargs = get_kwargs()
+
+    if len(keys) == 1:
+        assert keys[0] in kwargs, "Allowed options are '%s'" % ("', '".join(kwargs))
+        print(kwargs[keys[0]])
+    else:
+        for key, res in kwargs.items():
+            if isinstance(res, str) and "\n" in res:
+                res = '"""\n' + res + '\n"""'
+            elif isinstance(res, str):
+                res = '"' + res + '"'
+            elif isinstance(res, list) and res:
+                res = "[\n" + ",\n".join((repr(i) for i in res)) + "\n]"
+            else:
+                res = repr(res)
+
+            res = res.replace("\n", "\n |  ")
+            if not keys or key in keys:
+                print("%s: %s\n" % (key, res))
