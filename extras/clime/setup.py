6399bd8f44b1e5b538bc8fa8bc4532f29a66ea9a
Simone Bacchio <s.bacchio@gmail.com>
2020-03-30 15:57:45 +0300

    adding test

--- extras/clime/setup.py
+++ extras/clime/setup.py
@@ -3,6 +3,7 @@ from lyncs_setuptools import setup, CMakeExtension
 setup(
     'lyncs_clime',
     ext_modules = [CMakeExtension('lyncs_clime.clime', '.')],
+    data_files=[("tests", ["tests/conf.1000"])],
     install_requires = [
         "lyncs-cppyy",
     ],
