832b7425a292cf75e50a22979471a2d36f6a4c54
Simone Bacchio <s.bacchio@gmail.com>
2020-04-06 12:32:39 +0300

    Using libmpi and libclime

--- extras/DDalphaAMG/lyncs_DDalphaAMG/lib.py
+++ extras/DDalphaAMG/lyncs_DDalphaAMG/lib.py
@@ -4,15 +4,21 @@ __all__ = [
 ]
 
 from lyncs_cppyy import Lib
+from lyncs_mpi import lib as libmpi
 from . import __path__
-from .config import MPI_INCLUDE_DIRS
+from .config import WITH_CLIME
+
+if WITH_CLIME:
+    from lyncs_clime import lib as libclime
+else:
+    libclime = None
 
 PATHS = list(__path__)
+
 lib = Lib(
     path=PATHS,
-    include=MPI_INCLUDE_DIRS.split(";"),
     header="DDalphaAMG.h",
-    library="libDDalphaAMG.so",
-    c_include=False,
+    library=["libDDalphaAMG.so", libmpi, libclime],
+    c_include=True,
     check="DDalphaAMG_init",
 )
--- extras/DDalphaAMG/setup.py
+++ extras/DDalphaAMG/setup.py
@@ -2,14 +2,16 @@ try:
     from lyncs_setuptools import setup, CMakeExtension
     from lyncs_clime import __path__ as lime_path
 except:
-    print("""
+    print(
+        """
     
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     Install first the requirements:
     pip install -r requirements.txt
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     
-    """)
+    """
+    )
 
 setup(
     "lyncs_DDalphaAMG",
@@ -18,7 +20,7 @@ setup(
         CMakeExtension("lyncs_DDalphaAMG.lib", ".", ["-DLIME_PATH=%s" % lime_path[0]])
     ],
     data_files=[(".", ["config.py.in"])],
-    install_requires=["mpi4py", "lyncs-cppyy", "lyncs-clime",],
+    install_requires=["lyncs-mpi", "lyncs-cppyy", "lyncs-clime",],
     keywords=[
         "Lyncs",
         "DDalphaAMG",
