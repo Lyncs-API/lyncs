c1fcbbdd7f444649b9ba502a3606a34592e82750
Simone Bacchio <s.bacchio@gmail.com>
2020-04-13 15:10:03 +0300

    Adding documentation and small fixes

--- lyncs/field/base.py
+++ lyncs/field/base.py
@@ -1,29 +1,55 @@
 """
-Basic utils for the Field class.
-- implements the interface to the Lattice
+Base class of the Field type that implements
+the interface to the Lattice class.
 """
 
-
-from logging import warning
-from types import MappingProxyType
 from collections import OrderedDict
 from .types.base import FieldType
-from ..utils import default_repr
+from ..utils import default_repr, FrozenDict
 
 
 class BaseField:
+    """
+    Base class of the Field type that implements
+    the interface to the Lattice class and deduce
+    the list of Field types from the field axes.
+    
+    The list of types are accessible via field.types
+    """
+
     __repr__ = default_repr
 
-    def __init__(self, axes, lattice=None, types=None, **kwargs):
+    def __init__(self, field=None, axes=None, lattice=None, **kwargs):
+        """
+        The base field class.
+        
+        Parameters
+        ----------
+        field: instance of BaseField
+            If given, then the missing parameters are deduced from it.
+        axes: list(str)
+            List of axes of the field.
+        lattice: Lattice
+            The lattice on which the field is defined.
+        kwargs: dict
+           Extra parameters that will be passed to the field types.
+        """
+
         from ..lattice import Lattice, default_lattice
 
         assert lattice is None or isinstance(
             lattice, Lattice
         ), "lattice must be of Lattice type"
-        self._lattice = (lattice or default_lattice()).freeze()
-        self._axes = self.lattice.expand(axes)
 
-        types = types or FieldType.s
+        if isinstance(field, BaseField):
+            types = field.types
+            self._lattice = (lattice or field.lattice).freeze()
+            self._axes = self.lattice.expand(axes or field.axes)
+        else:
+            types = FieldType.s
+            self._lattice = (lattice or default_lattice()).freeze()
+            self._axes = self.lattice.expand(axes or ())
+
         self._types = OrderedDict()
         for name, ftype in types.items():
             if isinstance(self, ftype):
@@ -32,23 +58,26 @@ class BaseField:
                     getattr(ftype, "__init__").__get__(self)(**kwargs)
                 except AttributeError:
                     continue
-        self._types = MappingProxyType(self._types)
+        self._types = FrozenDict(self._types)
 
     @property
     def lattice(self):
+        "The lattice on which the field is defined."
         return self._lattice
 
     @property
     def axes(self):
+        "List of axes of the field. Order is not significant. See field.axes_order."
         return self._axes
 
     @property
     def types(self):
+        "List of field types that the field is instance of."
         return self._types
 
     def __getattr__(self, key):
         "Looks up for methods in the field types"
-        for ftype in self.types:
+        for ftype in self.types.values():
             if isinstance(self, ftype):
                 try:
                     return getattr(ftype, key).__get__(self)
@@ -56,6 +85,17 @@ class BaseField:
                     continue
 
         raise AttributeError
+
+    @property
+    def name(self):
+        "Name of the Field. Equivalent to the most relevant field type."
+        return [
+            name
+            for name, ftype in sorted(
+                self.types.items(),
+                key=lambda item: self.lattice.expand(item[1].axes.expand),
+            )
+        ][-1]
 
 
 FieldType.Field = BaseField
