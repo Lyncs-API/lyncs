a179395a673432d20e36af9419dd35a6a0e25d7d
Simone Bacchio <s.bacchio@gmail.com>
2020-05-07 13:30:37 +0300

    Adding add_kwargs_of

--- lyncs/utils/class_utils.py
+++ lyncs/utils/class_utils.py
@@ -6,17 +6,18 @@ __all__ = [
     "to_list",
     "default_repr",
     "add_parameters_to_doc",
+    "add_kwargs_of",
     "compute_property",
 ]
 
 from copy import copy
+from inspect import signature, _empty
 
 
 def default_repr(self):
     """
     Default repr used by lyncs
     """
-    from inspect import signature, _empty
 
     ret = (self.type if hasattr(self, "type") else type(self).__name__) + "("
     pad = " " * (len(ret))
@@ -55,13 +56,79 @@ def add_parameters_to_doc(doc, doc_params):
     found = False
     for i, line in enumerate(doc):
         words = line.split()
-        if words and "Parameters" == line.split()[0]:
+        if not found and len(words) == 1 and words[0].startswith("Parameter"):
             found = True
-        if found and not words:
+        elif found and not words:
             doc.insert(i, doc_params)
             return "\n".join(doc)
 
     return "\n".join(doc) + doc_params
+
+def get_parameters_doc(doc):
+    """
+    Extracts the documentation of the parameters
+    """
+    found = False
+    parameters = []
+    for i, line in enumerate(doc.split("\n")):
+        words = line.split()
+        if not found and len(words) == 1 and words[0].startswith("Parameter"):
+            found = True
+        elif found and words:
+            parameters.append(line)
+        elif found and not words:
+            break
+
+    if found and parameters:
+        return "\n".join(parameters[1:])
+    return doc
+
+
+def add_kwargs_of(fnc):
+    """
+    Decorator for adding kwargs of a function to another
+    """
+
+    def decorator(fnc2):
+        args = []
+        var_kwargs = False
+        kwargs = []
+
+        for key, val in signature(fnc2).parameters.items():
+            if val.kind == val.POSITIONAL_ONLY or (
+                val.kind == val.POSITIONAL_OR_KEYWORD and val.default == _empty
+            ):
+                args.append(key)
+            elif val.kind == val.VAR_POSITIONAL:
+                args.append("*"+key)
+            elif val.kind == val.VAR_KEYWORD:
+                var_kwargs = key
+            else:
+                kwargs.append((key, val.default))
+
+        assert (
+            var_kwargs is not False
+        ), "Cannot append kwargs to a function without **kwargs."
+
+        keys = [key for key,val in kwargs]
+        kwargs += [
+            (key, val.default)
+            for key, val in signature(fnc).parameters.items()
+            if val.kind in [val.POSITIONAL_OR_KEYWORD, val.KEYWORD_ONLY]
+            and val.default != _empty
+            and key not in keys
+        ]
+
+        args1 = list(args)
+        args.extend(("%s=%s" % (key,val) for key,val in kwargs))
+        args.append("**"+var_kwargs)
+        
+        args = ", ".join(args)
+        fnc2.__dict__["__wrapped__"] = eval("lambda %s: None" % (args))
+        fnc2.__doc__ = add_parameters_to_doc(fnc.__doc__, get_parameters_doc(fnc2.__doc__))
+        return fnc2
+
+    return decorator
 
 
 def to_list(*args):
