ce005d8cf9e1f758c35510956289f5b72d37d9ff
Simone Bacchio <s.bacchio@gmail.com>
2020-05-20 10:19:34 +0300

    Running black

--- setup.py
+++ setup.py
@@ -4,34 +4,22 @@ install_requires = [
     "dask",
     "numpy",
     "xmltodict",
-    ]
+    "tunable",
+]
 
 extras_require = {
     # lyncs/extras
     "clime": ["lyncs-clime"],
     "DDalphaAMG": ["lyncs-DDalphaAMG"],
-    
     # Groups
-    "cpu": [
-        "lyncs[DDalphaAMG]",
-    ],
-    
-    "gpu": [
-    ],
-    
-    "io": [
-        "lyncs[clime]",
-    ],
+    "cpu": ["lyncs[DDalphaAMG]",],
+    "gpu": [],
+    "io": ["lyncs[clime]",],
 }
 
-setup('lyncs',
-      install_requires = install_requires,
-      extras_require = extras_require,
-      keywords= [
-          "Python",
-          "API",
-          "Lattice",
-          "Field",
-          "QCD",
-      ],
-    )
+setup(
+    "lyncs",
+    install_requires=install_requires,
+    extras_require=extras_require,
+    keywords=["Python", "API", "Lattice", "Field", "QCD",],
+)
