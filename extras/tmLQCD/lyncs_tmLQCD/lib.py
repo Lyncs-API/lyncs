e4ddd84b1022e716e1540c2d19c4720508419f28
Simone Bacchio <s.bacchio@gmail.com>
2020-07-29 15:54:25 +0300

    Renaming symbols during installation

--- extras/tmLQCD/lyncs_tmLQCD/lib.py
+++ extras/tmLQCD/lyncs_tmLQCD/lib.py
@@ -14,14 +14,22 @@ from . import __path__
 
 PATHS = list(__path__)
 
-header = [
+headers = [
+    "global.h",
+    "rational/elliptic.h",
     "measure_gauge_action.h",
 ]
+
+redefined = {}
+with open(__path__[0] + "/lib/redefine-syms.txt", "r") as fp:
+    redefined.update((line.split() for line in fp.readlines()))
 
 
 lib = Lib(
     path=PATHS,
+    header=headers,
     library=["libtmLQCD.so", libclime],
     c_include=True,
     check="measure_plaquette",
+    redefined=redefined,
 )
