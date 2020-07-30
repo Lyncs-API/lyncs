d1e496981cff763d4e06798a254f615bfe6361f5
Simone Bacchio <s.bacchio@gmail.com>
2020-07-30 18:22:09 +0300

    Adding initialize to lib

--- extras/tmLQCD/lyncs_tmLQCD/lib.py
+++ extras/tmLQCD/lyncs_tmLQCD/lib.py
@@ -7,15 +7,53 @@ __all__ = [
     "PATHS",
 ]
 
-from lyncs_cppyy import Lib
+from lyncs_cppyy import Lib, nullptr
 from lyncs_clime import lib as libclime
 
 from . import __path__
+
+
+class tmLQCDLib(Lib):
+    __slots__ = [
+        "_initialized",
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._initialized = False
+
+    @property
+    def initialized(self):
+        return self._initialized
+
+    def initialize(self, x, y, z, t):
+        if self.initialized:
+            if not (x, y, z, t) == (self.LX, self.LY, self.LZ, self.T):
+                raise RuntimeError(
+                    f"""
+                tmLQCD has been already initialized with
+                (x,y,z,t) = {(self.LX, self.LY, self.LZ, self.T)}
+                and cannot be initialized again.
+                """
+                )
+            return
+        self.LX, self.LY, self.LZ, self.T_global = x, y, z, t
+        self.tmlqcd_mpi_init(0, nullptr)
+        self.init_geometry_indices(self.VOLUMEPLUSRAND)
+        self.geometry()
+        self.init_gauge_field(self.VOLUMEPLUSRAND, 0)
+        self._initialized = True
+
 
 PATHS = list(__path__)
 
 headers = [
     "global.h",
+    "start.h",
+    "mpi_init.h",
+    "geometry_eo.h",
+    "init/init_geometry_indices.h",
+    "init/init_gauge_field.h",
     "rational/elliptic.h",
     "measure_gauge_action.h",
 ]
@@ -25,7 +63,7 @@ with open(__path__[0] + "/lib/redefine-syms.txt", "r") as fp:
     redefined.update((line.split() for line in fp.readlines()))
 
 
-lib = Lib(
+lib = tmLQCDLib(
     path=PATHS,
     header=headers,
     library=["libtmLQCD.so", libclime],
