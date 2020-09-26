cb83b8e724a0bb0c9dc1052a53c9fe3b0a941b59
Simone Bacchio <s.bacchio@gmail.com>
2020-09-26 20:11:15 +0300

    Updating __init__

--- lyncs/__init__.py
+++ lyncs/__init__.py
@@ -3,20 +3,22 @@ Lyncs, a python API for LQCD applications
 """
 __version__ = "0.0.0"
 
+from .lattice import *
+
+from . import field
 from . import io
 
-from .lattice import *
-from .field import *
-from .io import *
-from . import utils
+import lyncs_utils as utils
 
-for extra in [
-    "lyncs_mpi",
-    "lyncs_cppyy",
-    "lyncs_clime",
-    "lyncs_DDalphaAMG",
+for pkg in [
+    "mpi",
+    "cppyy",
+    "clime",
+    "DDalphaAMG",
+    "tmLQCD",
 ]:
+    assert pkg not in globals(), f"{pkg} already defined"
     try:
-        exec("import %s as %s" % (extra, extra[6:]))
+        exec(f"import lyncs_{pkg} as {pkg}")
     except ModuleNotFoundError:
         pass
