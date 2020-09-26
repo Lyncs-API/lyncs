2c2f1050e3338b32c0669746e5b6e2ce88ff29ca
Simone Bacchio <s.bacchio@gmail.com>
2020-09-26 20:07:33 +0300

    Removing old tests

--- test/utils/test_factors.py
+++
@@ -1,9 +0,0 @@
-import numpy
-from lyncs.utils import prime_factors, factors
-
-
-def test_factors():
-    nums = [1, 20, 30, 123, 211]
-    for num in nums:
-        assert numpy.prod(list(prime_factors(num))) == num
-        assert numpy.prod(list(factors(num))) % num == 0
