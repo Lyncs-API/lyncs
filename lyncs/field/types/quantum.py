4f4c30bc44785762a8b4aed2a0b4c2aeacfe6015
Simone Bacchio <s.bacchio@gmail.com>
2020-09-27 15:50:52 +0300

    Adding plaquette

--- lyncs/field/types/quantum.py
+++ lyncs/field/types/quantum.py
@@ -17,7 +17,26 @@ class GaugeMatrix(Gauge):
 
 
 class GaugeLinks(Links, GaugeMatrix):
-    pass
+    def plaquette(self, dirs=None):
+        dirs = dirs or self.get_range("dirs")
+        if not set(dirs).issubset(self.get_range("dirs")):
+            raise ValueError("Dirs not part of the field")
+        if not len(dirs) > 1:
+            raise ValueError("At least two dirs needed for computing plaquette")
+
+        plaq = tuple(
+            self[dir1].dot(
+                self[dir2].roll(-1, dir1),
+                self[dir1].roll(-1, dir2).H,
+                self[dir2].H,
+                trace=True,
+                axes="all",
+                mean=True,
+            )
+            for i, dir1 in enumerate(dirs[:-1])
+            for dir2 in dirs[i + 1 :]
+        ).real
+        return plaq[0].add(plaq[1:]) / len(plaq)
 
 
 class Spinor(Scalar):
